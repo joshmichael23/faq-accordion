8c4be938866caf3008baa7a2e9d69d87ae6d250a
Josh Michael Olea <34050740+joshmichael23@users.noreply.github.com>
2023-12-27 14:52:14 +0800

    2023-12-17 finish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import logo from './logo.svg';
 import desktop from './images/illustration-woman-online-desktop.svg';
 import mobile from './images/illustration-woman-online-mobile.svg';
 import './style.css';
+import { useState } from 'react';
 
 function App() {
+  const [sectionOne, setSectionOne] = useState(false);
+  const [sectionTwo, setSectionTwo] = useState(false);
+  const [sectionThree, setSectionThree] = useState(false);
+  const [sectionFour, setSectionFour] = useState(false);
+  const [sectionFive, setSectionFive] = useState(false);
+  
+
   return (
     <div className="App">
       <div className='wrapper'>
@@ -17,33 +25,43 @@ function App() {
           <h1>FAQ</h1>
 
           <section>
-          <h2>How many team members can I invite</h2><button className='arrowdown'>V</button>
-          <p> You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</p>
+            <label for="sectionOne">
+              <h2 className={sectionOne ? 'active': ''}>How many team members can I invite</h2>
+              <button id="sectionOne" className={sectionOne ? 'arrowdown active': 'arrowdown'} onClick={()=> setSectionOne(sectionOne => !sectionOne)}></button>
+            </label>
+            <p className={sectionOne ? 'show': ''}> You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</p>
           </section>
 
           <section>
-          <h2>What is the maximum file upload size?</h2><button className='arrowdown'>V</button>
-          <p className='show'>No more than 2GB. All files in your account must fit your allotted storage space.</p>
+            <label>
+              <h2 className={sectionTwo ? 'active': ''}>What is the maximum file upload size?</h2>
+              <button className={sectionTwo ? 'arrowdown active': 'arrowdown'} onClick={()=> setSectionTwo(sectionTwo => !sectionTwo)}></button>
+            </label>
+            <p className={sectionTwo ? 'show': ''}>No more than 2GB. All files in your account must fit your allotted storage space.</p>
           </section>
 
           <section>
-          <h2>What is the maximum file upload size?</h2><button className='arrowdown'>V</button>
-          <p>No more than 2GB. All files in your account must fit your allotted storage space.</p>
+          <label>
+            <h2 className={sectionThree ? 'active': ''}> How do I reset my password?</h2>
+            <button className={sectionThree ? 'arrowdown active': 'arrowdown'} onClick={()=> setSectionThree(sectionThree => !sectionThree)}></button>
+          </label>
+          <p className={sectionThree ? 'show': ''}>Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.</p>
           </section>
 
           <section>
-          <h2>How do I reset my password?</h2><button className='arrowdown'>V</button>
-          <p>Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.</p>
+          <label>
+            <h2 className={sectionFour ? 'active': ''}>Can I cancel my subscription?</h2>
+            <button className={sectionFour ? 'arrowdown active': 'arrowdown'} onClick={()=> setSectionFour(sectionFour => !sectionFour)}></button>
+          </label>
+          <p className={sectionFour ? 'show': ''}>Yes! Send us a message and we’ll process your request no questions asked.</p>
           </section>
 
           <section>
-          <h2>Can I cancel my subscription?</h2><button className='arrowdown'>V</button>
-          <p>Yes! Send us a message and we’ll process your request no questions asked.</p>
-          </section>
-
-          <section>
-          <h2>Do you provide additional support?</h2><button className='arrowdown'>V</button>
-          <p>Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>
+          <label>
+            <h2 className={sectionFive ? 'active': ''}>Do you provide additional support?</h2>
+            <button className={sectionFive ? 'arrowdown active': 'arrowdown'} onClick={()=> setSectionFive(sectionFive => !sectionFive)}></button>
+          </label>
+          <p className={sectionFive ? 'show': ''}>Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>
           </section>
 
 
